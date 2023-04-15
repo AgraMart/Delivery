635225215ea572aded6a9083ebbff5a46fd9eaca
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2023-04-15 18:23:44 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import './App.css';
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 const [value, setValue] = useState(null)
+const [id,setId] = useState(null)
+
 const submitHandler =()=>{
-  console.log(value)
-}
+  axios
+      .post('http://localhost:8000/updateDelivery', {
+        id:id,
+        delivery: value
+      })
+      .then(res => {
+        toast.success('Verified Successful');
+      })
+      .catch(err => {
+        toast.error(`Failed`);
+      });
+  }
+
 
 
   return (
     <div className="App">
+      <ToastContainer />
       <div className="form">
 
             <label htmlFor="">Verify</label>
-            <input placeholder="Enter here..."  value={value}  onChange={e=>setValue(e.target.value)} />
+            <input placeholder="Enter Delivery ID..."  value={value}  onChange={e=>setValue(e.target.value)} />
+            <input placeholder="Enter Order ID..."  value={id}  onChange={e=>setId(e.target.value)} />
                 <button onClick={submitHandler} >Submit</button>
       </div>
                </div>
