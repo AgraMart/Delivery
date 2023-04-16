6ca7a3f25375695f33d981b14cc953b188d13d2d
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2023-04-16 23:01:19 +0530

    Readme

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const [id,setId] = useState(null)
 
 const submitHandler =()=>{
   axios
-      .post('http://34.131.68.37/updateDelivery', {
+      .post(' https://8aa0-117-196-234-150.ngrok-free.app/updateDelivery', {
         id:id,
         delivery: value
       })
